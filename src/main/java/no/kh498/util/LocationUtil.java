1f2a07d3a60ef18ad392f1ebdee20e175c898859
Karl Henrik Elg <klink100@gmail.com>
2017-11-15 03:06:18 +0100

    Add locationUtil

---
+++ src/main/java/no/kh498/util/LocationUtil.java
@@ -0,0 +1,38 @@
+package no.kh498.util;
+
+import org.bukkit.Location;
+
+import static java.lang.String.valueOf;
+
+/**
+ * @author kh498
+ */
+public class LocationUtil {
+
+    /**
+     * Round the xyz values of a location
+     *
+     * @param location
+     *     Location to round
+     *
+     * @return The location's XYZ values set to {@code location.getBlockX/Y/Z()}
+     */
+    public static Location roundLocation(final Location location) {
+        location.setX(location.getBlockX());
+        location.setY(location.getBlockY());
+        location.setZ(location.getBlockZ());
+        location.setYaw(0);
+        location.setPitch(0);
+        return location;
+    }
+
+    /**
+     * @param location
+     *     The location to beautify
+     *
+     * @return A string in the format of "{@code x, y, z}" excluding the quotation marks
+     */
+    public static String beautify(final Location location) {
+        return valueOf(location.getBlockX()) + ", " + location.getBlockY() + ", " + location.getBlockZ();
+    }
+}

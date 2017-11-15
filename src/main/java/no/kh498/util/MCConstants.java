f737b784e91bdad9f0f0b291d974b704bd1eb31b
Karl Henrik Elg <klink100@gmail.com>
2017-11-15 06:57:40 +0100

    Add methods to convert ticks and milliseconds

--- src/main/java/no/kh498/util/MCConstants.java
+++ src/main/java/no/kh498/util/MCConstants.java
@@ -4,6 +4,7 @@ package no.kh498.util;
  * @author karl henrik
  * @since 0.1.0
  */
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class MCConstants {
 
     /**
@@ -22,4 +23,18 @@ public class MCConstants {
     public static final long ONE_MINUTE_IN_MILLIS = ONE_SECOND_IN_TICKS * 60L * TICK_TO_MILLIS_FACTOR;
     public static final long TEN_MINUTES_IN_MILLIS = ONE_MINUTE_IN_MILLIS * 10L;
     public final static long TEN_SECONDS_IN_TICKS = ONE_SECOND_IN_TICKS * 10L;
+
+    /**
+     * Convert ticks to milliseconds
+     */
+    public static long ticksToMS(final long ticks) {
+        return ticks * TICK_TO_MILLIS_FACTOR;
+    }
+
+    /**
+     * Convert milliseconds to ticks
+     */
+    public static long msToTicks(final long ms) {
+        return ms / TICK_TO_MILLIS_FACTOR;
+    }
 }

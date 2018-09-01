7002ecf98063e32e4c1da84956dbb6751e38826b
Karl Henrik Elg <klink100@gmail.com>
2018-09-01 15:46:11 +0200

    Add option to get world using it's name

--- src/main/java/no/kh498/util/ConfigUtil.java
+++ src/main/java/no/kh498/util/ConfigUtil.java
@@ -19,12 +19,13 @@ import java.util.UUID;
 
 public class ConfigUtil {
 
-    private static final String WORLD_UID = "WORLD_UID";
-    private static final String X = "X";
-    private static final String Y = "Y";
-    private static final String Z = "Z";
-    private static final String YAW = "YAW";
-    private static final String PITCH = "PITCH";
+    public static final String WORLD_UID = "world_uid";
+    public static final String WORLD_NAME = "world_name";
+    public static final String X = "x";
+    public static final String Y = "y";
+    public static final String Z = "z";
+    public static final String YAW = "yaw";
+    public static final String PITCH = "pitch";
 
     private static final Logger logger = LoggerFactory.getLogger(ConfigUtil.class);
 
@@ -116,8 +117,23 @@ public class ConfigUtil {
      * @return A ConfigurationSection containing the serialized location
      */
     public static ConfigurationSection locationToConfig(Location location) {
+        return locationToConfig(location, true);
+    }
+
+    /**
+     * Save the precise location with pitch and yaw
+     *
+     * @param location
+     *     The location to save
+     *
+     * @return A ConfigurationSection containing the serialized location
+     */
+    public static ConfigurationSection locationToConfig(Location location, boolean useUUID) {
         ConfigurationSection conf = new YamlConfiguration();
-        conf.set(WORLD_UID, location.getWorld().getUID());
+
+        if (useUUID) { conf.set(WORLD_UID, location.getWorld().getUID()); }
+        else { conf.set(WORLD_NAME, location.getWorld().getName()); }
+
         conf.set(X, location.getX());
         conf.set(Y, location.getY());
         conf.set(Z, location.getZ());
@@ -130,7 +146,21 @@ public class ConfigUtil {
      * @return The location saved in {@code conf}
      */
     public static Location locationFromConfig(ConfigurationSection conf) {
-        World world = Bukkit.getWorld(UUID.fromString(conf.getString(WORLD_UID)));
+        return locationFromConfig(conf, true);
+    }
+
+    /**
+     * @return The location saved in {@code conf}
+     */
+    public static Location locationFromConfig(ConfigurationSection conf, boolean useUUID) {
+        World world;
+        if (useUUID) { world = Bukkit.getWorld(UUID.fromString(conf.getString(WORLD_UID))); }
+        else { world = Bukkit.getWorld(conf.getString(WORLD_NAME)); }
+
+        if (world == null && !useUUID) {
+            return null;
+        }
+
         double x = conf.getDouble(X);
         double y = conf.getDouble(Y);
         double z = conf.getDouble(Z);
@@ -149,8 +179,23 @@ public class ConfigUtil {
      * @return A ConfigurationSection containing the serialized location
      */
     public static ConfigurationSection blockLocationToConfig(Location location) {
+        return blockLocationToConfig(location, true);
+    }
+
+    /**
+     * Save the location as precise to the nearest block, without yaw and pitch
+     *
+     * @param location
+     *     The location to save
+     *
+     * @return A ConfigurationSection containing the serialized location
+     */
+    public static ConfigurationSection blockLocationToConfig(Location location, boolean useUUID) {
         ConfigurationSection conf = new YamlConfiguration();
-        conf.set(WORLD_UID, location.getWorld().getUID());
+
+        if (useUUID) { conf.set(WORLD_UID, location.getWorld().getUID()); }
+        else { conf.set(WORLD_NAME, location.getWorld().getName()); }
+
         conf.set(X, location.getBlockX());
         conf.set(Y, location.getBlockY());
         conf.set(Z, location.getBlockZ());
@@ -161,7 +206,19 @@ public class ConfigUtil {
      * @return The location saved in {@code conf}
      */
     public static Location blockLocationFromConfig(ConfigurationSection conf) {
-        World world = Bukkit.getWorld(UUID.fromString(conf.getString(WORLD_UID)));
+        return blockLocationFromConfig(conf, true);
+    }
+
+    /**
+     * @return The location saved in {@code conf}
+     */
+    public static Location blockLocationFromConfig(ConfigurationSection conf, boolean useUUID) {
+        World world;
+        if (useUUID) { world = Bukkit.getWorld(UUID.fromString(conf.getString(WORLD_UID))); }
+        else { world = Bukkit.getWorld(conf.getString(WORLD_NAME)); }
+        if (world == null && !useUUID) {
+            return null;
+        }
         int x = conf.getInt(X);
         int y = conf.getInt(Y);
         int z = conf.getInt(Z);

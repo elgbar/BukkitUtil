f71bd920a2d43183e9532939541e6ec34b27945d
Karl Henrik Elg <klink100@gmail.com>
2018-09-28 21:36:58 +0200

    Make save and load return booleans

--- src/main/java/no/kh498/util/Saveable.java
+++ src/main/java/no/kh498/util/Saveable.java
@@ -9,11 +9,16 @@ public interface Saveable {
 
     /**
      * Called when a plugin wants to save and on {@link Plugin#onDisable()}
+     *
+     * @return If this object was saved successfully
      */
-    void save();
+    boolean save();
 
     /**
      * Called when a plugin wants to read changes from disk and on {@link Plugin#onEnable()}
+     *
+     * @return If this object was loaded successfully
      */
-    void load();
+    boolean load();
+
 }

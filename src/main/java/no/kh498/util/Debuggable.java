c9b3f0d91246c176f43001520c01e1f4ed3b902a
Karl Henrik Elg <klink100@gmail.com>
2018-10-19 20:06:25 +0200

    Add helper methods for an uniform look

--- src/main/java/no/kh498/util/Debuggable.java
+++ src/main/java/no/kh498/util/Debuggable.java
@@ -1,6 +1,11 @@
 package no.kh498.util;
 
 import info.ronjenkins.slf4bukkit.ColorString;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.Collections;
 
 public interface Debuggable {
 
@@ -10,7 +15,7 @@ public interface Debuggable {
      *
      * @return the given StringBuilder, {@code sb}, and the last appended item must be a newline
      */
-    default ColorString appendDebugInfo(ColorString sb) {
+    default ColorString appendDebugInfo(@NotNull ColorString sb) {
         return appendDebugInfo(sb, "");
     }
 
@@ -22,13 +27,54 @@ public interface Debuggable {
      *
      * @return the given StringBuilder, {@code sb}, and the last appended item must be a newline (without the prefix)
      */
-    ColorString appendDebugInfo(ColorString sb, String prefix);
+    ColorString appendDebugInfo(@NotNull ColorString sb, @NotNull String prefix);
 
-    /**
-     *
-     * @return
-     */
     default String showDebugInfo() {
         return appendDebugInfo(new ColorString()).toString();
+    }
+
+
+    default void appendSingle(@NotNull ColorString sb, @NotNull String prefix, @NotNull String objectName,
+                              boolean bool) {
+        sb.none(prefix).yellow(getClass().getSimpleName() + ": " + objectName + " ? ");
+        
+        if (bool) { sb.green("true"); }
+        else { sb.red("false"); }
+
+        sb.none("\n");
+    }
+
+    default void appendSingle(@NotNull ColorString sb, @NotNull String prefix, @NotNull String objectName,
+                              @Nullable Object object) {
+        sb.none(prefix).yellow(getClass().getSimpleName() + ": " + objectName + " = ").aqua(object + "").none("\n");
+    }
+
+    default void appendDebuggable(@NotNull ColorString sb, @NotNull String prefix, @NotNull String objectName,
+                                  @Nullable Debuggable object) {
+        if (object == null) { appendSingle(sb, prefix, objectName, null); }
+        else { appendDebuggableList(sb, prefix, objectName, Collections.singleton(object));}
+    }
+
+    default void appendDebuggableList(@NotNull ColorString sb, @NotNull String prefix, @NotNull String objectName,
+                                      @Nullable Collection<? extends Debuggable> objects) {
+
+        sb.none(prefix).yellow(getClass().getSimpleName() + ": " + objectName + " = \n");
+        if (objects == null) {
+            sb.none("null");
+            return;
+        }
+
+        String objPrefix = prefix + "\t";
+
+        for (Debuggable debuggable : objects) {
+
+            sb.gold("\t" + debuggable.getClass().getSimpleName());
+            if (debuggable instanceof Nameable) {
+                sb.gold(" " + ((Nameable) debuggable).getName());
+            }
+            sb.none("\n");
+
+            debuggable.appendDebugInfo(sb, objPrefix).none("\n");
+        }
     }
 }

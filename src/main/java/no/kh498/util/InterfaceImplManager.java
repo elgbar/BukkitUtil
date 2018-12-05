fa7ea5f2678445ef4709b6e4eb8794ea97319e3b
Karl Henrik Elg <klink100@gmail.com>
2018-12-05 01:35:32 +0100

    Rename InterfaceMap to InterfaceImplManager
    To better reflect what it does

--- src/main/java/no/kh498/util/InterfaceImplManager.java
+++ src/main/java/no/kh498/util/InterfaceImplManager.java
@@ -15,22 +15,42 @@ import java.util.Map;
 import java.util.TreeMap;
 
 /**
- * This class is a utility class in the valentine realms quest plugin. It is responsible for holding references to
- * different parts of a quest.
- * <p>
- * When a quest references an objective, reward, precondition or post-cleanup it must also somehow have a reference to
- * it's class. This is the utility that converts a string to the given class.
+ * This utility class is responsible for holding references to different implementations of an interface. It is
+ * intended to hold all known implementation of an interface. It is useful when you want to write implementations where
+ * there might be dependencies that add their own implementation. For example a kit plugin can have multiple kits, if an
+ * independent wanted to add a kit they could use either {@link #registerClass(Class)} or {@link
+ * #registerFromPackage(Plugin, String)} to let the plugin know of its implementation.
+ *
+ * <h2>Usage</h2>
+ *
+ * <ol>
+ * <li>The constructor takes an interface as its argument (it <b>MUST</b> be an interface)</li>
+ * <li>Let the instance know of different implementation either directly with {@link #registerClass(Class)} or by
+ * scanning a package
+ * with{@link
+ * #registerFromPackage(Plugin, String)} </li>
+ * <li>
+ * To use the registered classes you can use
+ * <ol>
+ *
+ * <li>Get a list of all know implementations with {@link #getImplementations()}</li>
+ * <li>Get an instance of one of the implementations with {@link #getInstance(String)}</li>
+ * </ol>
+ * </li>
+ * </ol>
+ *
+ *
  * <p>
  * To register more classes you can do so either directly with {@link #registerClass(Class)} or by scanning a package
  * with {@link #registerFromPackage(Plugin, String)}
  *
  * @param <I>
- *     The interface that this class will hold a static reference to
+ *     The interface that this class will hold a reference to
  */
-public class InterfaceMap<I> {
+public class InterfaceImplManager<I> {
 
 
-    private static final Logger logger = LoggerFactory.getLogger(InterfaceMap.class);
+    private static final Logger logger = LoggerFactory.getLogger(InterfaceImplManager.class);
     private final Class<I> interfaceClass;
 
     private Map<String, Class<? extends I>> parts;
@@ -40,13 +60,15 @@ public class InterfaceMap<I> {
      * @throws IllegalArgumentException
      *     If the {@code clazz} is not an interface
      */
-    public InterfaceMap(Class<I> clazz) {
+    public InterfaceImplManager(Class<I> clazz) {
         Preconditions.checkArgument(Modifier.isInterface(clazz.getModifiers()), "The type class must be an interface");
         interfaceClass = clazz;
         parts = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
     }
 
     /**
+     * Notify the existence of all classes at the {@code plugin}s {@code package path}
+     * <p>
      * Scan classes in this path and look for classes implementing {@link I}
      */
     public void registerFromPackage(Plugin plugin, String packagePath) {
@@ -67,8 +89,8 @@ public class InterfaceMap<I> {
                 boolean success = registerClass(type);
                 if (success) { registered++; }
             }
-            logger.debug("Registering {} out of {} {} subclasses", registered, subTypes.size(),
-                         interfaceClass.getSimpleName());
+            logger.debug("Registering {} out of {} {} subclasses from package {}", registered, subTypes.size(),
+                         interfaceClass.getSimpleName(), packagePath);
         }
     }
 
@@ -112,8 +134,7 @@ public class InterfaceMap<I> {
 
         try {
             return parts.get(simpleName).getConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException
-            e) {
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
             throw new RuntimeException("Failed to create a new instance of '" + simpleName +
                                        "'. The constructor must not contain any arguments and must be public");
         }
@@ -122,7 +143,7 @@ public class InterfaceMap<I> {
     /**
      * @return A copy of the internal map
      */
-    public Map<String, Class<? extends I>> getParts() {
+    public Map<String, Class<? extends I>> getImplementations() {
         return Collections.unmodifiableMap(parts);
     }
 

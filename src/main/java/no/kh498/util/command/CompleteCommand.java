f4400735d77b6757e83d2ad5686cfe4db7e79e7b
Karl Henrik Elg <klink100@gmail.com>
2018-12-05 01:54:52 +0100

    Better checking and JavaDoc for CompleteCommands constructor

--- src/main/java/no/kh498/util/command/CompleteCommand.java
+++ src/main/java/no/kh498/util/command/CompleteCommand.java
@@ -4,6 +4,7 @@ import com.google.common.base.Preconditions;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.slf4j.Logger;
@@ -11,28 +12,74 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public abstract class CompleteCommand extends HostCommand implements TabCompleter {
 
     protected static final Logger logger = LoggerFactory.getLogger(CompleteCommand.class);
 
     /**
-     * Register this command to the plugin
+     * Register this command to the plugin, with a five ticks check delay.
      *
      * @param plugin
      *     The plugin to register this command to
+     *
+     * @throws NullPointerException
+     *     if given {@code plugin} is {@code null}
+     * @throws IllegalArgumentException
+     *     if {@code checkDelay} is less than {@code 1}
+     * @throws NullPointerException
+     *     if {@link #getAliases()} is {@code null}
+     * @throws IllegalArgumentException
+     *     if {@link #getAliases()} contains a {@code null} element
      */
     public CompleteCommand(JavaPlugin plugin) {
+        this(plugin, 5L);
+    }
+
+    /**
+     * Register this command to the plugin.
+     * <p>
+     * This constructor is not recommended to be used unless you get a timing error when {@link #verifySubcommands}
+     *
+     * @param checkDelay
+     *     How long to delay before verifying subcommands, must be greater than 0
+     * @param plugin
+     *     The plugin to register this command to
+     *
+     * @throws NullPointerException
+     *     if given {@code plugin} is {@code null}
+     * @throws IllegalArgumentException
+     *     if {@code checkDelay} is less than {@code 1}
+     * @throws NullPointerException
+     *     if {@link #getAliases()} is {@code null}
+     * @throws IllegalArgumentException
+     *     if {@link #getAliases()} contains a {@code null} element or is empty
+     * @throws IllegalStateException
+     *     if the first argument in {@link #getAliases()} is not registered in the plugins plugin.yml
+     */
+    public CompleteCommand(JavaPlugin plugin, long checkDelay) {
         super(null);
         Preconditions.checkNotNull(plugin, "A valid java plugin instance is needed to register this command");
+        Preconditions.checkNotNull(getAliases(), "getAliases() cannot be null");
+        Preconditions.checkArgument(!getAliases().isEmpty(), "There must be at least one alias");
+        Preconditions
+            .checkArgument(getAliases().stream().noneMatch(Objects::isNull), "None of the aliases can be null");
 
         String command = getAliases().iterator().next();
 
-        plugin.getCommand(command).setExecutor(this);
-        plugin.getCommand(command).setTabCompleter(this);
+        PluginCommand pluginCommand = plugin.getCommand(command);
+
+        if (pluginCommand == null) {
+            throw new IllegalStateException(
+                "Failed to set executor and tab completer as '" + command + "' is not a recognized command in plugin " +
+                plugin.getName() + ". Please define it in plugin.yml");
+        }
+        pluginCommand.setExecutor(this);
+        pluginCommand.setTabCompleter(this);
 
         //verify all subcommands when they are all registered
-        Bukkit.getScheduler().runTaskLater(plugin, this::verifySubcommands, 2L);
+        Bukkit.getScheduler().runTaskLater(plugin, this::verifySubcommands, checkDelay);
     }
 
     @Override

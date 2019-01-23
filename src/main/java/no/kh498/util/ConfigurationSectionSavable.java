cd0908433614e9ac941648b2b949d83b29a34e36
Karl Henrik Elg <klink100@gmail.com>
2019-01-23 19:28:29 +0100

    Add nullable/notnull annotations to ConfigurationSectionSavable

--- src/main/java/no/kh498/util/ConfigurationSectionSavable.java
+++ src/main/java/no/kh498/util/ConfigurationSectionSavable.java
@@ -1,21 +1,31 @@
 package no.kh498.util;
 
+import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.ConfigurationSection;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
 
 public interface ConfigurationSectionSavable {
 
+    String ROOT_PATH = File.separator;
 
     /**
-     * @return this instance as a ConfigurationSection
+     * @return this instance as a ConfigurationSection or {@code null} if there is nothing to save
      */
+    @Nullable
     ConfigurationSection toConfig();
 
     /**
      * @param conf
      *     The section to load from
+     * @param root
+     *     The root configuration of {@code conf}
+     * @param absolutePath
+     *     Path to {@code conf} from {@code root}, if at root the path should be displayed as {@link #ROOT_PATH}
      *
      * @return if the configuration was successfully loaded
      */
-    //TODO make this return a List<String> of keys NOT used
-    boolean fromConfig(ConfigurationSection conf);
+    boolean fromConfig(@NotNull ConfigurationSection conf, @NotNull Configuration root, @NotNull String absolutePath);
 }

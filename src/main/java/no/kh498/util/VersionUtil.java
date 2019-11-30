e83202d06d6d9f50d0083a02a37153d81afe0429
Karl Henrik Elg <klink100@gmail.com>
2019-11-30 16:11:09 +0100

    Add all nms versions from 1.7 to 1.14.4

--- src/main/java/no/kh498/util/VersionUtil.java
+++ src/main/java/no/kh498/util/VersionUtil.java
@@ -7,11 +7,28 @@ import org.bukkit.Bukkit;
  */
 public class VersionUtil {
 
+    public static final String v1_14_R1 = "v1_14_R1";
+
+    public static final String v1_13_R2 = "v1_13_R2";
+    public static final String v1_13_R1 = "v1_13_R1";
+
     public static final String v1_12_R1 = "v1_12_R1";
+
+    public static final String v1_11_R1 = "v1_11_R1";
+
+    public static final String v1_10_R1 = "v1_10_R1";
+
+    public static final String v1_9_R2 = "v1_9_R2";
+    public static final String v1_9_R1 = "v1_9_R1";
 
     public static final String v1_8_R3 = "v1_8_R3";
     public static final String v1_8_R2 = "v1_8_R2";
     public static final String v1_8_R1 = "v1_8_R1";
+
+    public static final String v1_7_R4 = "v1_7_R4";
+    public static final String v1_7_R3 = "v1_7_R3";
+    public static final String v1_7_R2 = "v1_7_R2";
+    public static final String v1_7_R1 = "v1_7_R1";
 
     public static final String CB_PACKAGE = "org.bukkit.craftbukkit";
 

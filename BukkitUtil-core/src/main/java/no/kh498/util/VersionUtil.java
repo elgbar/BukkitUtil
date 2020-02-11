4c12503aa9f5592e43e98a1884f4145647631c6d
Karl Henrik Elg <klink100@gmail.com>
2020-02-11 22:20:52 +0100

    Add 1.15 to list of versions

--- BukkitUtil-core/src/main/java/no/kh498/util/VersionUtil.java
+++ BukkitUtil-core/src/main/java/no/kh498/util/VersionUtil.java
@@ -7,6 +7,8 @@ import org.bukkit.Bukkit;
  */
 public class VersionUtil {
 
+    public static final String v1_15_R1 = "v1_15_R1";
+
     public static final String v1_14_R1 = "v1_14_R1";
 
     public static final String v1_13_R2 = "v1_13_R2";

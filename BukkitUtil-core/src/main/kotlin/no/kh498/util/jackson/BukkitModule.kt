8104e85d1a093483b13d37eab105b07332bf9541
Karl Henrik Elg <klink100@gmail.com>
2020-03-16 17:06:31 +0100

    Add dependency on guava module
    Bukkit/mojang uses guava so it should be included in this module

--- BukkitUtil-core/src/main/kotlin/no/kh498/util/jackson/BukkitModule.kt
+++ BukkitUtil-core/src/main/kotlin/no/kh498/util/jackson/BukkitModule.kt
@@ -3,10 +3,11 @@ package no.kh498.util.jackson
 import com.fasterxml.jackson.core.Version
 import com.fasterxml.jackson.databind.Module
 import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.datatype.guava.GuavaModule
 import no.kh498.util.jackson.deserializers.BukkitDeserializers
-import no.kh498.util.jackson.serializers.BukkitSerializers
 import no.kh498.util.jackson.deserializers.ColoredStringDeserializer
 import no.kh498.util.jackson.mixIn.*
+import no.kh498.util.jackson.serializers.BukkitSerializers
 import org.bukkit.Color
 import org.bukkit.Location
 import org.bukkit.World
@@ -37,5 +38,9 @@ class BukkitModule(
         context.setMixInAnnotations(Color::class.java, ColorMixIn::class.java)
         context.setMixInAnnotations(World::class.java, WorldMixIn::class.java)
         context.setMixInAnnotations(Location::class.java, LocationMixIn::class.java)
+    }
+
+    override fun getDependencies(): List<GuavaModule> {
+        return listOf(GuavaModule())
     }
 }

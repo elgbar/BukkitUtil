d96ba1d6541de33bcfa4e032889a30a1d5a70435
Karl Henrik Elg <klink100@gmail.com>
2020-03-17 18:00:23 +0100

    Filter out default values for item meta

--- BukkitUtil-core/src/main/kotlin/no/kh498/util/jackson/serializers/BukkitSerializers.kt
+++ BukkitUtil-core/src/main/kotlin/no/kh498/util/jackson/serializers/BukkitSerializers.kt
@@ -18,6 +18,7 @@ import org.bukkit.inventory.meta.ItemMeta
 class BukkitSerializers(val bukkitModule: BukkitModule) : Serializers.Base() {
     override fun findSerializer(config: SerializationConfig, type: JavaType, beanDesc: BeanDescription): JsonSerializer<*>? {
         return when {
+
             ItemMeta::class.java.isAssignableFrom(type.rawClass) -> {
 
                 class ItemMetaSerializer : StdSerializer<ItemMeta>(ItemMeta::class.java) {
@@ -25,18 +26,34 @@ class BukkitSerializers(val bukkitModule: BukkitModule) : Serializers.Base() {
                     val mapper: ObjectMapper = ObjectMapper().registerModule(BukkitModule(bukkitModule.colorizeStringsByDefault, true))
                     val mapType: MapType = mapper.typeFactory.constructMapType(MutableMap::class.java, String::class.java, Any::class.java)
 
-                    override fun serialize(value: ItemMeta, gen: JsonGenerator, provider: SerializerProvider) {
+                    override fun serialize(meta: ItemMeta, gen: JsonGenerator, provider: SerializerProvider) {
                         val map = LinkedHashMap<String, Any?>()
 
-                        map[ItemMetaMixIn.TYPE_FIELD] = ItemMetaMixIn.classMap[value::class.java]
-                                ?: error("Unknown item meta ${value::class.java}. Perhaps you are using an unsupported version of BukkitUtil?")
+                        map[ItemMetaMixIn.TYPE_FIELD] = ItemMetaMixIn.classMap[meta::class.java]
+                                ?: error("Unknown item meta ${meta::class.java}. Perhaps you are using an unsupported version of BukkitUtil?")
 
 
                         //the spigot class is weird so we add it as an extra class
-                        map.putAll(mapper.convertValue<MutableMap<String, Any?>>(value.spigot(), mapType))
-                        map.putAll(mapper.convertValue<MutableMap<String, Any?>>(value, mapType))
+                        map.putAll(mapper.convertValue<MutableMap<String, Any?>>(meta.spigot(), mapType))
+                        map.putAll(mapper.convertValue<MutableMap<String, Any?>>(meta, mapType))
 
-                        gen.writeObject(map)
+                        //filter out default values
+                        gen.writeObject(map.filterValues { value ->
+                            when (value) {
+                                null -> false
+                                is Collection<*> -> value.isNotEmpty()
+                                is Map<*, *> -> value.isNotEmpty()
+                                is String -> value.isNotEmpty()
+                                is Int? -> value != 0
+                                is Long? -> value != 0
+                                is Float? -> value != 0f
+                                is Boolean? -> value != false
+                                is Double? -> value != 0.0
+                                is Byte? -> value != 0
+                                is Short? -> value != 0
+                                else -> true
+                            }
+                        })
                     }
                 }
 

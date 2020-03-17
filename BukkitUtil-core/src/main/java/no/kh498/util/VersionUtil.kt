a48ee6236636ece604cc60a58dfe037369939cb3
Karl Henrik Elg <klink100@gmail.com>
2020-03-17 11:08:31 +0100

    Add methods to get version dependent classes
    Update power mockito But not on 2.0.x yet
    And create tests for version util

---
+++ BukkitUtil-core/src/main/java/no/kh498/util/VersionUtil.kt
@@ -0,0 +1,48 @@
+package no.kh498.util
+
+import org.bukkit.Bukkit
+
+/**
+ * @author Elg
+ */
+object VersionUtil {
+    const val v1_15_R1 = "v1_15_R1"
+    const val v1_14_R1 = "v1_14_R1"
+    const val v1_13_R2 = "v1_13_R2"
+    const val v1_13_R1 = "v1_13_R1"
+    const val v1_12_R1 = "v1_12_R1"
+    const val v1_11_R1 = "v1_11_R1"
+    const val v1_10_R1 = "v1_10_R1"
+    const val v1_9_R2 = "v1_9_R2"
+    const val v1_9_R1 = "v1_9_R1"
+    const val v1_8_R3 = "v1_8_R3"
+    const val v1_8_R2 = "v1_8_R2"
+    const val v1_8_R1 = "v1_8_R1"
+    const val v1_7_R4 = "v1_7_R4"
+    const val v1_7_R3 = "v1_7_R3"
+    const val v1_7_R2 = "v1_7_R2"
+    const val v1_7_R1 = "v1_7_R1"
+    const val BUKKIT_PACKAGE = "org.bukkit"
+    const val CB_PACKAGE = "$BUKKIT_PACKAGE.craftbukkit"
+    const val NMS_PACKAGE = "net.minecraft.server"
+
+    /**
+     * @return The net minecraft server version running this code in the form of 'v1_x_y_Rz'
+     */
+    @JvmStatic
+    val nmsVersion: String
+        get() = Bukkit.getServer().javaClass.getPackage().name.replace(CB_PACKAGE, "").replaceFirst(".".toRegex(), "")
+
+    @JvmStatic
+    @Throws(ClassNotFoundException::class)
+    fun getVersionedClass(packageName: String, className: String): Class<*> {
+        val fullClassPath = "$packageName.$nmsVersion.$className"
+        return Class.forName(fullClassPath)
+    }
+
+    @JvmStatic
+    @Throws(ClassNotFoundException::class)
+    fun getCBClass(className: String): Class<*> {
+        return getVersionedClass(CB_PACKAGE, className)
+    }
+}
---
+++ BukkitUtil-core/src/test/java/no/kh498/util/VersionUtilTest.kt
@@ -0,0 +1,53 @@
+package no.kh498.util
+
+import no.kh498.util.VersionUtil.CB_PACKAGE
+import no.kh498.util.VersionUtil.getCBClass
+import no.kh498.util.VersionUtil.getVersionedClass
+import no.kh498.util.VersionUtil.nmsVersion
+import no.kh498.util.VersionUtil.v1_8_R3
+import org.bukkit.Bukkit
+import org.bukkit.Server
+import org.bukkit.craftbukkit.v1_8_R3.CraftServer
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.api.mockito.PowerMockito.`when`
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+
+/**
+ * @author Elg
+ */
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(Bukkit::class, CraftServer::class)
+class VersionUtilTest {
+    @Before
+    fun setUp() {
+        PowerMockito.mockStatic(Bukkit::class.java)
+        `when`(Bukkit.getServer()).thenReturn(PowerMockito.mock(CraftServer::class.java))
+    }
+
+    @Test
+    fun nmsClass() {
+        assertNotNull(nmsVersion)
+    }
+
+    @Throws(ClassNotFoundException::class)
+    @Test
+    fun versionedClass() {
+        val expectedClass = Class.forName("$CB_PACKAGE.$nmsVersion.CraftServer")
+        assertNotNull(expectedClass)
+        assertEquals(expectedClass, getVersionedClass(CB_PACKAGE, "CraftServer"))
+    }
+
+    @Throws(ClassNotFoundException::class)
+    @Test
+    fun cBClass() {
+        val expectedClass = Class.forName("$CB_PACKAGE.$nmsVersion.CraftServer")
+        assertNotNull(expectedClass)
+        assertEquals(expectedClass, getCBClass("CraftServer"))
+    }
+}
